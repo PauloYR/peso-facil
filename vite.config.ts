b629a96bfd8ebb8695c2cfe24452ecf7461b849e
Paulo Rocha <pauloyr@outlook.com>
2025-05-05 19:18:49 -0300

    Configuração do deploy automático

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { ghPages } from 'vite-plugin-gh-pages'
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    ghPages()
   ],
+  base: 'vite-plugin-gh-pagesPauloYR/peso-facil',
 })
